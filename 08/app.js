c01f4647c24b70b6a267934661762384f8ef6da2
acisliver <7273322@naver.com>
2021-05-02 00:47:30 +0900

    08

---
+++ 08/app.js
@@ -0,0 +1,19 @@
+// const promise = fetch("http");
+
+// console.log(promise);
+
+// promise.then(res =>{
+//     console.log('res:', res);
+//     const text = res.text();
+//     text.then(res=> {
+//         console.log('text:', res);
+//     })
+
+// })
+// const html = fetch("http")
+// .then(res => res.text())
+// .then(text => console.log(text))
+
+function callback(data){
+    console.log("callback: ", data)
+}
