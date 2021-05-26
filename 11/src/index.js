79ec1140aebabfa7421dd92a59b79ca4cdd32fb0
acisliver <7273322@naver.com>
2021-05-26 18:33:33 +0900

    11

---
+++ 11/src/index.js
@@ -0,0 +1,26 @@
+import './todos.css';
+
+import { render } from './html-render';
+import formInput from './form-input';
+
+const $result = document.querySelector('#result');
+
+const todos = [];
+
+$result.addEventListener('click', (event) => {
+  let { className } = event.target;
+  if (className === 'delete') {
+    //todo 삭제
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === 'toggle-checked') {
+    //todo chech box 선택
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone; //toggle 반전
+    render(todos);
+  }
+});
+
+formInput.init(todos);
+render(todos);
